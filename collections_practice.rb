6130cd8edc7591870ef5ea1c768a82d30a4d6c93
Jack Goode <jack.w.goode@icloud.com>
2020-06-08 12:12:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,5 +22,17 @@ def sort_array_desc(array_of_int)
   end
  end
 end 
+
+def sort_array_asc(array_of_strings)
+array_of_strings.sort  do |a, b|
+  if a.length == b.length
+    0
+  elsif a.length < b.length
+     -1
+  elsif a.length > b.length
+      1
+  end
+ end
+end 
   
 
