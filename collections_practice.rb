1e219d1796fbf0a18cd7e543815a60b0d5062585
Jack Goode <jack.w.goode@icloud.com>
2020-06-08 11:42:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(int_array)
+  int_array.collect do |num|
+    yield()
+  
+  
+  
+  
+end
 
+sort_array_asc do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
+end
